a874da7a3a30a1be5991b12aac670d5f46b176e3
danielolivermoco <danielmoco10@gmail.com>
2024-05-31 23:30:59 -0300

    criação dos componentes do header

---
+++ src/components/SectionHero.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./Container";
+
+export function SectionHero() {
+    return (
+        <section className="w-full h-[704px] bg-img_bg_hero bg-no-repeat bg-center bg-cover">section hero
+        </section>
+
+    )
+}
